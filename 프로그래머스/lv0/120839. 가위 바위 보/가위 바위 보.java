238d90f8173ac52e3768f9fca723e5c60f92de9d
tlswltjq <41179427+tlswltjq@users.noreply.github.com>
2023-03-16 18:17:51 +0900

    [level 0] Title: 가위 바위 보, Time: 0.13 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String rsp) {
+        StringBuffer sb = new StringBuffer();
+        for(int i =0;i<rsp.length();i++){
+            if(rsp.charAt(i)=='2'){
+                sb.append('0');
+            } else if (rsp.charAt(i)=='0') {
+                sb.append('5');
+            }else{
+                sb.append('2');
+            }
+        }
+        return sb.toString();
+    }
+}

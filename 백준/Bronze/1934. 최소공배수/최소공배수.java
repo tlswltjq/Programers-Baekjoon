1cf220cd2daa70fb075349f4938656934fd86e0c
tlswltjq <41179427+tlswltjq@users.noreply.github.com>
2023-04-18 11:08:34 +0900

    [Bronze I] Title: 최소공배수, Time: 392 ms, Memory: 22428 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a, b;
+        for(int i = 0 ; i < n;i++){
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println(a*b/GCD(a, b));
+        }
+
+    }
+    public static int GCD(int a, int b){
+        while(b!=0){
+            int r =a%b;
+            a = b;
+            b= r;
+        }
+        return a;
+    }
+}     

c5f8d75dcf882c6433902394443388f66b638669
tlswltjq <41179427+tlswltjq@users.noreply.github.com>
2023-05-14 23:14:18 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,19 +1,26 @@
 #include <iostream>
 using namespace std;
+long long recursion(long long a, long long b, long long c);
 
-long long pow_mod(long long a, long long b, long long c);
+int main(){
+	long long a, b, c;
 
-int main() {
-    long long a, b, c;
-    cin >> a >> b >> c;
-    cout << pow_mod(a, b, c) << endl;
-    return 0;
+	cin>> a>> b>> c;
+	cout<< recursion(a, b, c);
+
+	return 0;
 }
 
-long long pow_mod(long long a, long long b, long long c) {
-    if (b == 0) return 1;
-    long long temp = pow_mod(a, b/2, c);
-    temp = (temp * temp) % c;
-    if (b % 2 == 1) temp = (temp * a) % c;
-    return temp;
+long long recursion(long long a, long long b, long long c){
+	if (b==0){
+		return 1;
+	}
+	long long tmp = recursion(a, b/2, c);
+	tmp = (tmp * tmp)%c;
+
+	if (b%2 == 1){
+		tmp = (tmp * a)%c;
+	}
+
+	return tmp;
 }

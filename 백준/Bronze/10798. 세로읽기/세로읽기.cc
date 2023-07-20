cbd522170d63ab0763494920ddd0edd4f4a3478b
tlswltjq <41179427+tlswltjq@users.noreply.github.com>
2023-07-20 17:09:22 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.cc
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -1,20 +1,16 @@
 #include<iostream>
 using namespace std;
 char str[5][16];
-int mx=0;
 int main(){
     string s;
     for(int i = 0 ; i <5 ; i++){
         cin>> s;
-        if (mx<s.length()){
-            mx = s.length();
-        }
         for(int j = 0 ; j < s.length(); j++){
             str[i][j]=s[j];
         }
     }
 
-    for(int j = 0 ; j <mx; j++){
+    for(int j = 0 ; j <16; j++){
         for(int i = 0 ; i <5; i++){
             if(str[i][j]!=0){
                 cout<<str[i][j];

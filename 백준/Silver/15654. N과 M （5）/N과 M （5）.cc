01e15c5c0bbd46c98d7c14038cc4e75e9d9300e6
tlswltjq <41179427+tlswltjq@users.noreply.github.com>
2023-08-21 20:20:40 +0900

    [Silver III] Title: N과 M (5), Time: 44 ms, Memory: 2120 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, m;
+int answer[10];
+bool chk[100000];
+int nums[10];
+
+void back_t(int k){
+  if (k==m){
+    for(int i = 0; i< m; i++){
+      cout<<answer[i]<<" ";
+    }
+    cout<<"\n";
+    return;
+  }
+  for(int i = 0 ; i<n; i++){
+    if(!chk[nums[i]]){
+      answer[k] = nums[i];
+      chk[nums[i]] = true;
+      back_t(k+1);
+      chk[nums[i]] = false;
+    }
+  }
+
+}
+
+int main(){
+  cin>> n>>m;
+  for(int i = 0 ; i < n; i++){
+    cin>> nums[i];
+  }
+sort(nums, nums+n);
+  back_t(0);
+  return 0;
+}
